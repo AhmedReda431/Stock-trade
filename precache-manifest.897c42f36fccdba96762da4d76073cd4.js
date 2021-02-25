a862af1dc568baafe1f8749efbb3f44107811599
Ahmed Reda <48368885+AhmedReda431@users.noreply.github.com>
2021-02-25 15:04:12 +0200

    Add files via upload

---
+++ precache-manifest.897c42f36fccdba96762da4d76073cd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff8dcb22780109c2b06",
+    "url": "/css/app.cdc5291b.css"
+  },
+  {
+    "revision": "2eb4705013e31f7c6765",
+    "url": "/css/chunk-vendors.52eaf30c.css"
+  },
+  {
+    "revision": "46eb78ed9e224e9c5125ad876c6592a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ff8dcb22780109c2b06",
+    "url": "/js/app.88528e4b.js"
+  },
+  {
+    "revision": "2eb4705013e31f7c6765",
+    "url": "/js/chunk-vendors.8d1ba062.js"
+  },
+  {
+    "revision": "680764a0087cab994bf5468f2ded3cb3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
